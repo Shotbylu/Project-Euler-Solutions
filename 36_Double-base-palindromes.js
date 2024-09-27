17110b800a3467f3a4950019f8f8b4696eb5cde2
Shotbylu <lungelosibisi83@gmail.com>
2024-09-27 14:35:02 +0200

    Update 36_Double-base-palindromes.js
    There is a bug in the JavaScript code due to the way the variables n and k are extracted and used. Both are initially extracted as strings from the input, but they should be converted to numbers before using them in calculations or loops. Specifically:
    
    n is used in a loop, but it's a string, so it needs to be converted to a number.
    k is used in the toString(k) method to convert numbers to base k, but it also needs to be converted to a number.

--- 36_Double-base-palindromes.js
+++ 36_Double-base-palindromes.js
@@ -1,30 +1,38 @@
 function processData(input) 
 {
-    var n = input.split(' ')[0];
-    var k = input.split(' ')[1];
+    // Convert n and k to integers
+    var n = parseInt(input.split(' ')[0]);
+    var k = parseInt(input.split(' ')[1]);
     var ans = 0;
     
-    for(var i=1; i<n; i++)
+    // Iterate from 1 to n-1
+    for(var i = 1; i < n; i++)
     {
-        var s = i + '';
+        var s = i.toString(); // Convert i to a string
 
+        // Check if the number is a palindrome in base-10
         if(s === s.split('').reverse().join(''))
         {
+            // Convert the number to base-k
             var converted = i.toString(k);
             
-            if(converted == converted.split('').reverse().join('')) ans += i; 
+            // Check if the base-k conversion is a palindrome
+            if(converted === converted.split('').reverse().join('')) 
+                ans += i; // Add the number to the result if both conditions are met
         }
     }
-    console.log(ans);
+    
+    console.log(ans); // Output the result
 } 
 
+// Handling input from stdin
 process.stdin.resume();
 process.stdin.setEncoding("ascii");
-_input = "";
+let _input = "";
 process.stdin.on("data", function (input) {
     _input += input;
 });
 
 process.stdin.on("end", function () {
-   processData(_input);
+   processData(_input); // Process the input once it ends
 });
