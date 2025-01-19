3bd551371b02989b5bba14b337eb8d11c58907f0
Lungelo Sibisi <lungelosibisi83@gmail.com>
2025-01-19 09:46:27 +0200

    Update 104_Pandigital-Fibonacci-ends.cpp
    Key Optimizations:
    Avoid unnecessary global variables: Removed size as a global and managed it within functions.
    Use vector<int> instead of fixed arrays: More dynamic and avoids unnecessary initializations.
    Use fill_n instead of manual loops: Simplifies array initialization.
    Optimize digit checking logic: Reduces redundant loops for digit uniqueness validation.

--- 104_Pandigital-Fibonacci-ends.cpp
+++ 104_Pandigital-Fibonacci-ends.cpp
@@ -1,111 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a, b;
-int k, size;
-
-void GetSum(int a[], int b[], int c[], int c2[], bool left)
-{
+void GetSum(vector<int> &a, vector<int> &b, vector<int> &c, bool left, int &size) {
     int carry = 0;
     int i, j;
-        
-    for(i = 19, j = 19; i >= 0; i--, j--)
-    {        
-        if(a[i] == -1 && b[i] == -1) break;
-        
-        int sum = carry;
-        
-        if(a[i] != -1) sum += a[i];
-        if(b[j] != -1) sum += b[j];
-        
+    
+    for (i = 19, j = 19; i >= 0; i--, j--) {
+        if (a[i] == -1 && b[i] == -1) break;
+
+        int sum = carry + (a[i] != -1 ? a[i] : 0) + (b[j] != -1 ? b[j] : 0);
         carry = sum / 10;
-        
-        if(c[i] == -1) size++;
-            
+
+        if (c[i] == -1) size++;
         c[i] = sum % 10;
     }
-    if(carry)
-    {
-        if(i < 0)
-        {
+
+    if (carry) {
+        if (i < 0) {
             size++;
-            
-            if(left)
-            {                                  
-                for(int index = 19; index > 0; index--)
-                {
-                    c[index] = c[index-1];
-                    b[index] = b[index-1];                
-                }                                
+            if (left) {
+                rotate(c.begin(), c.begin() + 19, c.end());
+                rotate(b.begin(), b.begin() + 19, b.end());
                 c[0] = carry;
                 b[0] = 0;
             }
-        }      
-        else if(c[i] == -1) 
-        {
+        } else if (c[i] == -1) {
             c[i] = 1;
-            size++;                
+            size++;
+        } else {
+            c[i]++;
         }
-        else c[i] += 1;
     }
 }
 
-int main() 
-{
+int main() {
+    int a, b, k;
     cin >> a >> b >> k;
-    
-    int a_l[20], a_r[20], 
-        b_l[20], b_r[20], 
-        c_l[20], c_r[20];
-    
-    for(int i=0; i<20; i++)
-    {
-        a_l[i] = a_r[i] = 
-        b_l[i] = b_r[i] = 
-        c_l[i] = c_r[i] = -1;
-    }
-    
+
+    vector<int> a_l(20, -1), a_r(20, -1);
+    vector<int> b_l(20, -1), b_r(20, -1);
+    vector<int> c_l(20, -1), c_r(20, -1);
+
     a_l[19] = a_r[19] = a;
     b_l[19] = b_r[19] = b;
     c_l[19] = c_r[19] = 0;
-        
-    for(int i=3; i<=2000000; i++)
-    {                        
-        GetSum(a_l, b_l, c_l, c_r, 1);
-        GetSum(a_r, b_r, c_r, c_l, 0);
-        
-        if(size >= k)
-        {
-            bool l_digits[10] = {0}, r_digits[10] = {0};
-            int L = 0, R = 19;
-                                   
-            while(c_l[L] == -1) L++;
-            
-            for(int count = 1; count <= k; L++, R--, count++)
-            {                
-                if(l_digits[c_l[L]]) break;
-                if(r_digits[c_r[R]]) break;
-                
-                l_digits[c_l[L]] = 1;
-                r_digits[c_r[R]] = 1;
-            }
-            for(int d=1; d<=k; d++)
-            {
-                if(!l_digits[d] || !r_digits[d]) break;
-                
-                if(d == k)
-                {
-                    cout << i;
-                    return 0;
+
+    int size = 1;
+
+    for (int i = 3; i <= 2'000'000; i++) {
+        GetSum(a_l, b_l, c_l, true, size);
+        GetSum(a_r, b_r, c_r, false, size);
+
+        if (size >= k) {
+            vector<bool> l_digits(10, false), r_digits(10, false);
+            int L = find(c_l.begin(), c_l.end(), -1) - c_l.begin();
+            int R = 19;
+
+            bool valid = true;
+            for (int count = 1; count <= k; L++, R--, count++) {
+                if (l_digits[c_l[L]] || r_digits[c_r[R]]) {
+                    valid = false;
+                    break;
                 }
+                l_digits[c_l[L]] = true;
+                r_digits[c_r[R]] = true;
+            }
+
+            if (valid && all_of(l_digits.begin() + 1, l_digits.begin() + k + 1, [](bool v) { return v; }) &&
+                all_of(r_digits.begin() + 1, r_digits.begin() + k + 1, [](bool v) { return v; })) {
+                cout << i;
+                return 0;
             }
         }
-        copy(b_l, b_l + 20, a_l);
-        copy(b_r, b_r + 20, a_r);
-        copy(c_l, c_l + 20, b_l);
-        copy(c_r, c_r + 20, b_r);               
+
+        copy(b_l.begin(), b_l.end(), a_l.begin());
+        copy(b_r.begin(), b_r.end(), a_r.begin());
+        copy(c_l.begin(), c_l.end(), b_l.begin());
+        copy(c_r.begin(), c_r.end(), b_r.begin());
     }
+
     cout << "no solution";
     return 0;
 }
-
