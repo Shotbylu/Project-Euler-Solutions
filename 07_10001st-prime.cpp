6e6aa89a00eec411364350c078af1f28b2b61bf4
Lungelo Sibisi <lungelosibisi83@gmail.com>
2025-01-27 08:36:25 +0200

    Update 07_10001st-prime.cpp

--- 07_10001st-prime.cpp
+++ 07_10001st-prime.cpp
@@ -1,71 +1,46 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
+vector<int> memo = {2};  // Memoized prime numbers
 
-vector<int> memo;
-
-int Primes(int n)
-{
-    vector<int> primes = {2};
-    vector<bool> sieve(n*n);
+int Primes(int n) {
+    int last_index = memo.size();
+    int last_prime = memo.back();
     
-    int p = 2;        
+    if (last_index >= n) return memo[n - 1];
 
-    while(primes.size() < n*n)
-    {                      
-        for(int i=2; i*p<n*n; i++)
-        {
-            if(sieve[p*i] == false)
-            {
-                sieve[p*i] = true;
-            }
-        }   
-        
-        int temp = p;
-        
-        for(int i=p; i<n*n; i++)
-        {            
-            if(i>p && sieve[i] == false)
-            {
-               primes.push_back(i);
-               p = i;
+    vector<bool> sieve(2 * n * log(n) + 100, true); // Approximate upper bound
+    sieve[0] = sieve[1] = false; // 0 and 1 are not prime
 
-               break;
-            }            
+    // Mark non-prime numbers
+    for (int p : memo) {  
+        for (int j = p * p; j < sieve.size(); j += p) {
+            sieve[j] = false;
         }
-        if(p == temp) break;
     }
-    out:
-    //for(auto it : primes) cerr << it << endl;
-    
-    memo = primes;       
-    return primes[n-1];    
+
+    for (int i = last_prime + 1; memo.size() < n; ++i) {
+        if (sieve[i]) {
+            memo.push_back(i);
+            for (int j = i * i; j < sieve.size(); j += i) {
+                sieve[j] = false;
+            }
+        }
+    }
+
+    return memo[n - 1];
 }
 
-
-
-int main() 
-{
+int main() {
     int t;
     cin >> t;
-    
-    while(t--)
-    {
+
+    while (t--) {
         int n;
         cin >> n;
-        
-        if(memo.size() >= n)
-        {
-            cout << memo[n-1] << endl;
-        }        
-        else 
-        {
-            cout << Primes(n) << endl;
-        }
+        cout << Primes(n) << endl;
     }
+
     return 0;
 }
